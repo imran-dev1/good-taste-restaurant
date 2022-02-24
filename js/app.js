723a4722f31f27cd318a1dac112b96a4e0c22be0
webdev-it <webdevit3@gmail.com>
2022-02-24 21:25:31 +0600

    added spinner

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+function spinnerControl(remove, add) {
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove(remove);
+  spinner.classList.add(add);
+}
 const searchBtn = document.getElementById("search-btn");
 searchBtn.addEventListener("click", () => {
   const searchField = document.getElementById("search-field");
@@ -8,6 +13,10 @@ searchBtn.addEventListener("click", () => {
     errorDiv.innerText = "😝 Please write something to search";
   } else {
     errorDiv.innerText = "";
+
+    // Spinner
+
+    spinnerControl("d-none", "d-block");
     // Load Meals APi ..........
 
     const loadMeals = () => {
@@ -30,6 +39,7 @@ searchBtn.addEventListener("click", () => {
 
     const displayMeals = (data) => {
       const resultContainer = document.getElementById("result-container");
+      spinnerControl("d-block", "d-none");
       const mealsFromData = data.meals;
       const resultCount = document.getElementById("result-count");
       if (mealsFromData.length > 1) {
@@ -63,6 +73,9 @@ searchBtn.addEventListener("click", () => {
     };
   }
 });
+
+// Meal Details Modal
+
 const mealDetails = (mealId) => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
   fetch(url)
@@ -75,7 +88,9 @@ const displayMealDetails = (meal) => {
   const modalContent = document.createElement("div");
   modalContent.classList.add("row", "g-0");
   modalContent.innerHTML = `
-      <div class="col-md-4" style="background-image:url('${meal.strMealThumb}');background-repeat: no-repeat; background-position: center center; background-size: cover;">
+      <div class="col-md-4" style="background-image:url('${
+        meal.strMealThumb
+      }');background-repeat: no-repeat; background-position: center center; background-size: cover;">
           <div style="height:300px"></div>
       </div>
       <div class="col-md-8">
@@ -84,8 +99,14 @@ const displayMealDetails = (meal) => {
               <h3 class="card-title">${meal.strMeal}</h3>
               <p class="area">Type: <span>${meal.strArea}</span></p>
               <p class="category">Category: <span>${meal.strCategory}</span></p>
-              <p class="card-text">${meal.strInstructions}</p>
-              <p class="ingredients">Ingredients: <span>${meal.strIngredient1}</span><span>${meal.strIngredient2}</span><span>${meal.strIngredient3}</span><span>${meal.strIngredient4}</span><span>${meal.strIngredient5}</span><span>${meal.strIngredient6}</span></p>
+              <p class="card-text">${meal.strInstructions.slice(0, 500)}...</p>
+              <p class="ingredients">Ingredients: <span>${
+                meal.strIngredient1
+              }</span><span>${meal.strIngredient2}</span><span>${
+    meal.strIngredient3
+  }</span><span>${meal.strIngredient4}</span><span>${
+    meal.strIngredient5
+  }</span><span>${meal.strIngredient6}</span></p>
           </div>
       </div>
   `;
